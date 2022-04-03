78568fb20b04a8beecdf4a8c5a7c19864c40ae26
Michiel Borkent <michielborkent@gmail.com>
2022-04-03 12:55:12 +0200

    write json

--- test/clj_easy/tools/misc/json_test.clj
+++ test/clj_easy/tools/misc/json_test.clj
@@ -15,6 +15,6 @@
       (is (= {:a 1} (json/read-str "{\"a\": 1}")))
       (is (= {:a 1} (json/read-str "{\"a\": 1}" {:key-fn keyword})))
       (is (= {"a" 1} (json/read-str "{\"a\": 1}" {:key-fn str}))))
-    (testing "read json"
+    (testing "write json"
       (is (= [1 2 3] (json/read-str (json/write-str [1 2 3]))))
       (is (= {:a 1} (json/read-str (json/write-str {:a 1})))))))
